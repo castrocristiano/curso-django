0502752bbf87fb21431da17abf61a06780cdeeed
Cristiano da Silva Castro <cscastro87@gmail.com>
2020-06-28 00:58:55 -0300

    Adjust code identation

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from simplemooc.core.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
 ]
+
