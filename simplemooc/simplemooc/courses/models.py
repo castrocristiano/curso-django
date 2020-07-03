81db547ddf412fcc5e8436849a4ac5ef01d8cd99
Cristiano da Silva Castro <cscastro87@gmail.com>
2020-07-02 23:50:30 -0300

    Creating entities

---
+++ simplemooc/simplemooc/courses/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+'''
+    Herança: Todas as classes que representam entidades de banco devem herdar de models.Model.
+    
+    Para preparar as tabelas para geração, executar o comando: python manage.py makemigrations
+    Para gerar, executar o comando: migrate --run-syncdb
+'''
+
+
+class Course(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Atalho')
+    description = models.TextField('Descrição', blank=True)
+    start_date = models.DateField(
+        'Data de Início',
+        null=True,
+        blank=True
+    )
+    image = models.ImageField(
+        upload_to='courses/images',
+        verbose_name='Imagem'
+    )
+    create_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated = models.DateTimeField('Atualizado em', auto_now_add=True)
