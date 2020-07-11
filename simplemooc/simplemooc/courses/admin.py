8d69d3bdecb39ee3f8dd45ae8f04bd265d2b8350
Cristiano da Silva Castro <cscastro87@gmail.com>
2020-07-11 00:48:15 -0300

    CRUD admin

--- simplemooc/simplemooc/courses/admin.py
+++ simplemooc/simplemooc/courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_date', 'created_at', 'id']
+    search_fields = ['id', 'name', 'slug']
+    prepopulated_fields = {'slug': ['name']}
+
+
+admin.site.register(Course, CourseAdmin)
+
